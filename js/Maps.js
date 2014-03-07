9e4d51fd15257034604043e1dda545b6050753d8
Dave Michelson <dmichelson@ashevillenc.gov>
2014-03-07 11:39:20 -0500

    added a couple of days
    add monday and tuesday.  day maps need zoom to fit.  maps charts need
    hover maps need sorting.

--- js/Maps.js
+++ js/Maps.js
@@ -27,3 +27,26 @@ function renderMap(file,htmlid){
 
 
 	};
+
+
+	function renderSubMap(file,htmlid){
+
+      var url = file;
+
+      var map = L.mapbox.map(htmlid, 'examples.map-9ijuk24y')
+            .setView([35.55,-82.5495], 11);
+
+       $.ajax({
+          headers: {
+            'Accept': 'application/vnd.github.v3.raw'
+          },
+          dataType: 'json',
+          url: url,
+          success: function(geojson) {
+              // On success add fetched data to the map.
+              var feat = L.mapbox.featureLayer(geojson).addTo(map);
+          }
+        });      
+      
+
+	};
