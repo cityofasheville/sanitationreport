7f8bfea679248a10673d1d4af560cfba2ac14b03
Dave Michelson <dmichelson@ashevillenc.gov>
2014-03-10 11:07:24 -0400

    tweaking map rendering and chart rendering

--- js/Charts.js
+++ js/Charts.js
@@ -49,8 +49,18 @@ function renderChart(file,htmlid,label,value) {
                 return yScale(d[value]);
                })
                .attr("fill", function(d) {
-                return "rgb(0, 0, " + (d[value] * 10) + ")";
-               })
+                 return d[label] == 'MONDAY' ? '#e41a1c' :
+                       d[label] == 'TUESDAY' ? '#377eb8' :
+                       d[label] == 'WEDNESDAY' ? '#4daf4a' :
+                       d[label] == 'THRUSDAY'  ? '#a65628' :
+                       d[label] == 'TRUCK 1' ? '#e41a1c' :
+                       d[label] == 'TRUCK 2' ? '#377eb8' :
+                       d[label] == 'TRUCK 3'  ? '#4daf4a' :
+                       d[label] == 'TRUCK 4'  ? '#ff7f00' :
+                       d[label] == 'TRUCK 5'  ? '#984ea3' :
+                       d[label] == 'TRUCK 6'  ? '#a65628' :
+                                  '#FFEDA0';
+                           })
 
               //Tooltip
               .on("mouseover", function(d) {
@@ -112,192 +122,6 @@ function renderChart(file,htmlid,label,value) {
                .attr("fill", "white");
 
 
-           //  var sortOrder = false;
-           //  var sortBars = function () {
-           //      sortOrder = !sortOrder;
-
-           //      sortItems = function (a, b) {
-           //          if (sortOrder) {
-           //              return a[value] - b[value];
-           //          }
-           //          return b[value] - a[value];
-           //      };
-
-           //      svg.selectAll("rect")
-           //          .sort(sortItems)
-           //          .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .attr("x", function (d, i) {
-           //          return xScale(i);
-           //      });
-
-           //      svg.selectAll('text')
-           //          .sort(sortItems)
-           //          .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .text(function (d) {
-           //          return d[value];
-           //      })
-           //          .attr("text-anchor", "middle")
-           //          .attr("x", function (d, i) {
-           //          return xScale(i) + xScale.rangeBand() / 2;
-           //      })
-           //          .attr("y", function (d) {
-           //          return h - yScale(d[value]) + 14;
-           //      });
-           //  };
-           //  // Add the onclick callback to the button
-           // // d3.select("#sort").on("click", sortBars);
-           // // d3.select("#reset").on("click", reset);
-           //  function randomSort() {
-
-
-           //    svg.selectAll("rect")
-           //          .sort(sortItems)
-           //          .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .attr("x", function (d, i) {
-           //          return xScale(i);
-           //      });
-
-           //      svg.selectAll('text')
-           //          .sort(sortItems)
-           //          .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .text(function (d) {
-           //          return d[value];
-           //      })
-           //          .attr("text-anchor", "middle")
-           //          .attr("x", function (d, i) {
-           //          return xScale(i) + xScale.rangeBand() / 2;
-           //      })
-           //          .attr("y", function (d) {
-           //          return h - yScale(d[value]) + 14;
-           //      });
-           //  }
-           //  function reset() {
-           //    svg.selectAll("rect")
-           //      .sort(function(a, b){
-           //        return a[label] - b[label];
-           //      })
-           //      .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .attr("x", function (d, i) {
-           //          return xScale(i);
-           //      });
-
-           //    svg.selectAll('text')
-           //          .sort(function(a, b){
-           //        return a[label] - b[label];
-           //      })
-           //          .transition()
-           //          .delay(function (d, i) {
-           //          return i * 50;
-           //      })
-           //          .duration(1000)
-           //          .text(function (d) {
-           //          return d[value];
-           //      })
-           //          .attr("text-anchor", "middle")
-           //          .attr("x", function (d, i) {
-           //          return xScale(i) + xScale.rangeBand() / 2;
-           //      })
-           //          .attr("y", function (d) {
-           //          return h - yScale(d[value]) + 14;
-           //      });
-           //  };    
-
-
-              // var valueLabelWidth = 40; // space reserved for value labels (right)
-              // var barHeight = 20; // height of one bar
-              // var barLabelWidth = 100; // space reserved for bar labels
-              // var barLabelPadding = 5; // padding between bar and bar labels (left)
-              // var gridLabelHeight = 18; // space reserved for gridline labels
-              // var gridChartOffset = 3; // space between start of grid and first bar
-              // var maxBarWidth = 420; // width of the bar with the max value
-               
-              // // accessor functions 
-              // var barLabel = function(d) { return d[label]; };
-              // var barValue = function(d) { return parseFloat(d[value]); };
-             
-              // // sorting
-              // var sortedData = data.sort(function(a, b) {
-              //  return d3.descending(barValue(a), barValue(b));
-              // }); 
-
-              // // scales
-              // var yScale = d3.scale.ordinal().domain(d3.range(0, sortedData.length)).rangeBands([0, sortedData.length * barHeight]);
-              // var y = function(d, i) { return yScale(i); };
-              // var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
-              // var x = d3.scale.linear().domain([0, d3.max(sortedData, barValue)]).range([0, maxBarWidth]);
-              // // svg container element
-              // var chart = d3.select('#'+htmlid).append("svg")
-              //   .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
-              //   .attr('height', gridLabelHeight + gridChartOffset + sortedData.length * barHeight);
-              // // grid line labels
-              // var gridContainer = chart.append('g')
-              //   .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
-              // gridContainer.selectAll("text").data(x.ticks(10)).enter().append("text")
-              //   .attr("x", x)
-              //   .attr("dy", -3)
-              //   .attr("text-anchor", "middle")
-              //   .text(String);
-              // // vertical grid lines
-              // gridContainer.selectAll("line").data(x.ticks(10)).enter().append("line")
-              //   .attr("x1", x)
-              //   .attr("x2", x)
-              //   .attr("y1", 0)
-              //   .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
-              //   .style("stroke", "#ccc");
-              // // bar labels
-              // var labelsContainer = chart.append('g')
-              //   .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
-              // labelsContainer.selectAll('text').data(sortedData).enter().append('text')
-              //   .attr('y', yText)
-              //   .attr('stroke', 'none')
-              //   .attr('fill', 'black')
-              //   .attr("dy", ".35em") // vertical-align: middle
-              //   .attr('text-anchor', 'end')
-              //   .text(barLabel);
-              // // bars
-              // var barsContainer = chart.append('g')
-              //   .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
-              // barsContainer.selectAll("rect").data(sortedData).enter().append("rect")
-              //   .attr('y', y)
-              //   .attr('height', yScale.rangeBand())
-              //   .attr('width', function(d) { return x(barValue(d)); })
-              //   .attr('stroke', 'white')
-              //   .attr('fill', 'steelblue');
-              // // bar value labels
-              // barsContainer.selectAll("text").data(sortedData).enter().append("text")
-              //   .attr("x", function(d) { return x(barValue(d)); })
-              //   .attr("y", yText)
-              //   .attr("dx", 3) // padding-left
-              //   .attr("dy", ".35em") // vertical-align: middle
-              //   .attr("text-anchor", "start") // text-align: right
-              //   .attr("fill", "black")
-              //   .attr("stroke", "none")
-              //   .text(function(d) { return d3.round(barValue(d), 2); });
-              // // start line
-              // barsContainer.append("line")
-              //   .attr("y1", -gridChartOffset)
-              //   .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
-              //   .style("stroke", "#000");
 
           });
 }
--- js/Maps.js
+++ js/Maps.js
@@ -1,3 +1,4 @@
+
 function renderMap(file,htmlid){
 
       var url = file;
@@ -12,21 +13,40 @@ function renderMap(file,htmlid){
           dataType: 'json',
           url: url,
           success: function(geojson) {
+
+             feat = L.geoJson(geojson, {style: style}).addTo(map);
+             map.fitBounds(feat.getBounds());
+
               // On success add fetched data to the map.
-              var featureLayer = L.mapbox.featureLayer(geojson).addTo(map);
+              //var featureLayer = L.mapbox.featureLayer(geojson,{on:  function() { alert('Clicked on a group!'); }}).addTo(map);
+
+
+             feat.eachLayer(function(layer) {
+
+                  //here you call `bindPopup` with a string of HTML you create - the feature
+                  //properties declared above are available under `layer.feature.properties`
+
+                 // var content = '<h1>Pickup Day: ' + layer.feature.properties.theday + '<\/h1>' +
+                 //     '<h2>Hours: ' + ((layer.feature.properties.prediction/60)/60).toFixed(2).toString() + '<\/h2>' +
+                 //     '<h2>Hours for 6 Trucks: ' + (((layer.feature.properties.prediction/60)/60)/6).toFixed(2).toString() + '<\/h2>' +
+                 //     '<h2>Number of Cans: ' + layer.feature.properties.cans + '<\/h2>';;
+                 // layer.bindPopup(content);
+             });
+
+
           }
         });      
       
 
-        map.featureLayer.on('mouseover', function(e) {
-            e.layer.openPopup();
-        });
-        map.featureLayer.on('mouseout', function(e) {
-            e.layer.closePopup();
-        });
-
 
 	};
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        click: zoomToFeature
+    });
+}
+
 
 
 	function renderSubMap(file,htmlid){
@@ -44,11 +64,55 @@ function renderMap(file,htmlid){
           url: url,
           success: function(geojson) {
               // On success add fetched data to the map.
-              var feat = L.mapbox.featureLayer(geojson).addTo(map);
-              map.fitBounds(feat.getBounds());
+             feat = L.geoJson(geojson, {style: styletruck}).addTo(map);
+             map.fitBounds(feat.getBounds());
               
           }
         });      
       
 
 	};
+
+function zoomToFeature(e) {
+    alert(  e.target.getBounds()  )
+    map.fitBounds(e.target.getBounds());
+}
+
+  function getColor(d) {
+    return d == 'MONDAY' ? '#e41a1c' :
+           d == 'TUESDAY' ? '#377eb8' :
+           d == 'WEDNESDAY'  ? '#4daf4a' :
+           d == 'THRUSDAY'  ? '#a65628' :
+           d == 'TRUCK 1' ? '#e41a1c' :
+           d == 'TRUCK 2' ? '#377eb8' :
+           d == 'TRUCK 3'  ? '#4daf4a' :
+           d == 'TRUCK 4'  ? '#ff7f00' :
+           d == 'TRUCK 5'  ? '#984ea3' :
+           d == 'TRUCK 6'  ? '#a65628' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    
+    return {
+        fillColor: getColor(feature.properties.theday),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+
+}
+
+    function styletruck(feature) {
+    
+    return {
+        fillColor: getColor(feature.properties.thetruck),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+  }
