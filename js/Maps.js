b7de314d5ff04702c6203e294db0e84c8ad04c77
Dave Michelson <dmichelson@ashevillenc.gov>
2014-03-11 17:14:30 -0400

    added summary text

--- js/Maps.js
+++ js/Maps.js
@@ -9,6 +9,10 @@ function renderMap(file,htmlid,dataid){
             .setView([35.55,-82.5495], 10);
       var polyindex = 0;
 
+      function mapToTreeIds(feature){
+        return feature.properties.htmlid;
+      }
+
       $.ajax({
           headers: {
             'Accept': 'application/vnd.github.v3.raw'
@@ -17,17 +21,18 @@ function renderMap(file,htmlid,dataid){
           url: url,
           success: function(geojson) {
 
-             feat = L.geoJson(geojson, {style: style, onEachFeature: onEachFeature}).addTo(map);
+             feat = L.geoJson(geojson, {style: style, onEachFeature: onEachFeature }).addTo(map);
              map.fitBounds(feat.getBounds());
              for (each in feat._layers) {
-               var name = feat._layers[each].feature.properties.theday;
-               var swLat = feat._layers[each].getBounds()._southWest.lat;
-               var swLng = feat._layers[each].getBounds()._southWest.lng;
-               var neLat = feat._layers[each].getBounds()._northEast.lat;
-               var neLng = feat._layers[each].getBounds()._northEast.lng;
-               var bbox = name+"-"+swLat + "," + swLng + "," + neLat + "," + neLng+"\n";
+               //var name = feat._layers[each].feature.properties.theday;
+               //var swLat = feat._layers[each].getBounds()._southWest.lat;
+               //var swLng = feat._layers[each].getBounds()._southWest.lng;
+               //var neLat = feat._layers[each].getBounds()._northEast.lat;
+               //var neLng = feat._layers[each].getBounds()._northEast.lng;
+               //var bbox = name+"-"+swLat + "," + swLng + "," + neLat + "," + neLng+"\n";
 
-              $("#highlight"+dataid).append(bbox.toString())
+              //$("#highlight"+dataid).append(bbox.toString())
+              //feat.className = "id_"+feat._layers[each].feature.properties.theday+"_"+htmlid
              }
 
               // On success add fetched data to the map.
@@ -39,8 +44,15 @@ function renderMap(file,htmlid,dataid){
                     mouseover: highlightFeature,
                     mouseout: resetHighlight,
                     click: zoomToFeature
+                    //hover: highLightChart
                 });
             }
+
+
+            //function highLightChart(e){
+            //    alert('here')
+            //    $( "#id_MONDAY" ).trigger( "mouseenter" );
+            //}
 
             function highlightFeature(e) {
                 var layer = e.target;
@@ -52,6 +64,10 @@ function renderMap(file,htmlid,dataid){
                     fillOpacity: 0.7
                 });
 
+               // var c= $( "rect#id_MONDAY" );
+               //c.stroke('#666');
+
+                //$( "rect#id_MONDAY" ).trigger( "click",function(d){ alert(d3.select(this).attr('id') ) } );
                 if (!L.Browser.ie && !L.Browser.opera) {
                     layer.bringToFront();
                 }
@@ -216,14 +232,15 @@ function renderMap(file,htmlid,dataid){
 
 
 
-function style(feature) {
+function style(feature,htmlid) {
     
     return {
         fillColor: getColor(feature.properties.theday),
         weight: 2,
         opacity: 1,
         color: 'white',
-        fillOpacity: 0.7
+        fillOpacity: 0.7,
+        id: "id_"+feature.properties.theday 
     };
 
 }
